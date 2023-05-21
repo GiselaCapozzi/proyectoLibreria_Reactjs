21940c29524084ce6663e34a4d7aa4959f287749
Mario Ramb <marioramb@gmail.com>
2023-05-21 11:42:14 -0300

    add autores json

---
+++ src/data/autores.js
@@ -0,0 +1,91 @@
+export const autores = [
+    {
+        "id": 1,
+        "author_name": "Gabriel García Márquez",
+        "books": [
+            "Cien años de soledad",
+            "Crónica de una muerte anunciada",
+            "El amor en los tiempos del cólera"
+        ]
+    },
+    {
+        "id": 2,
+        "author_name": "Miguel de Cervantes Saavedra",
+        "books": [
+            "Don Quijote de la Mancha"
+        ]
+    },
+    {
+        "id": 3,
+        "author_name": "Julio Cortázar",
+        "books": [
+            "Rayuela"
+        ]
+    },
+    {
+        "id": 4,
+        "author_name": "Jorge Luis Borges",
+        "books": [
+            "Ficciones"
+        ]
+    },
+    {
+        "id": 5,
+        "author_name": "Juan Rulfo",
+        "books": [
+            "Pedro Páramo"
+        ]
+    },
+    {
+        "id": 6,
+        "author_name": "Carlos Ruiz Zafón",
+        "books": [
+            "La sombra del viento"
+        ]
+    },
+    {
+        "id": 7,
+        "author_name": "Roberto Bolaño",
+        "books": [
+            "Los detectives salvajes"
+        ]
+    },
+    {
+        "id": 8,
+        "author_name": "Isabel Allende",
+        "books": [
+            "La casa de los espíritus"
+        ]
+    },
+    {
+        "id": 9,
+        "author_name": "Mario Vargas Llosa",
+        "books": [
+            "La ciudad y los perros",
+            "La guerra del fin del mundo",
+            "Pantaleón y las visitadoras"
+        ]
+    },
+    {
+        "id": 10,
+        "author_name": "Pablo Neruda",
+        "books": [
+            "Veinte poemas de amor y una canción desesperada",
+            "Canto general"
+        ]
+    },
+    {
+        "id": 11,
+        "author_name": "Octavio Paz",
+        "books": [
+            "El laberinto de la soledad"
+        ]
+    },
+    {
+        "id": 12,
+        "author_name": "Gustavo Adolfo Bécquer",
+        "books": [
+            "Rimas y leyendas"
+        ]
+    }
+]
