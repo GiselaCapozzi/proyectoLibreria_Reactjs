a580bb1d699369350f78fbc287cf63474571163b
Gisselissa <gisecapozzi@gmail.com>
2023-05-22 13:14:46 -0300

    Trabajando en el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,40 @@
 import style from './Footer.module.css';
 
-const Footer = () => {
-  return (
-    <div className={`${style.flex_container} ${style.flex_end}`}>
-    <li className="flex-item">1</li>
-      <li className="flex-item">2</li>
-      <li className="flex-item">3</li>
-      <li className="flex-item">4</li>
-      <li className="flex-item">5</li>
-      <li className="flex-item">6</li>
-    </div>
-  )
+import { Component } from 'react'
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div className={`${style.contenedor}`}>
+        <div className={`${style.flex_container} ${style.flex_end}`}>
+          <div className={`${style.footer_novedades}`}>
+            <h4>Recibir Novedades y Promociones</h4>
+            <input type="email" name="" id="" />
+            <button>Suscribirse</button>
+          </div>
+          <div className={`${style.footer_seccion}`}>
+            <div className={`${style.footer_categoria}`}>
+              <section>
+                <p>CATEGORIAS LIBROS</p>
+              </section>
+            </div>
+            <div className={`${style.footer_contacto}`}>
+              <section>Quienes somos</section>
+            </div>
+            <div className={`${style.footer_micuenta}`}>
+              <section>MI CUENTA</section>
+            </div>
+            <div className={`${style.footer_seguinos}`}>
+              <section>SEGUINOS</section>
+            </div>
+          </div>
+          <div className={`${style.footer_derechos}`}>
+            <h3>©All Rights Reserved</h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
