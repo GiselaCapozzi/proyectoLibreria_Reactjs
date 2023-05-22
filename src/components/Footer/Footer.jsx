37455cbe895451a1c93ff610e721522f766d6b40
Gisselissa <gisecapozzi@gmail.com>
2023-05-22 17:54:11 -0300

    Footer PC

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,31 +1,68 @@
+import { Component } from 'react';
 import style from './Footer.module.css';
+import { Link } from 'react-router-dom';
 
-import { Component } from 'react'
-
+import { categorias } from '../../data/categorias';
+import instagram from '../../assets/instagram.png';
+import youtube from '../../assets/youtube.png';
+import facebook from '../../assets/facebook(1).png';
+import twitter from '../../assets/twitter.png';
 export class Footer extends Component {
+  
   render() {
     return (
       <div className={`${style.contenedor}`}>
         <div className={`${style.flex_container} ${style.flex_end}`}>
           <div className={`${style.footer_novedades}`}>
             <h4>Recibir Novedades y Promociones</h4>
-            <input type="email" name="" id="" />
-            <button>Suscribirse</button>
+            <div className={`${style.suscribe} form-floating mb-3`}>
+            <input className='form-control' type="email" id="floatingInput" placeholder='Email' />
+            <label htmlFor="floatingInput">Email</label>
+            <button className='btn btn-primary'>Suscribirse</button>
+            </div>
           </div>
           <div className={`${style.footer_seccion}`}>
             <div className={`${style.footer_categoria}`}>
               <section>
                 <p>CATEGORIAS LIBROS</p>
+                <div className={`${style.contenedor_categoria}`}>
+                {
+                  categorias.map(categoria => (
+                    <Link 
+                      key={categoria.id}
+                      className={style.lista_categoria}
+                    >{categoria.nombre}</Link>
+                  ))
+                }
+                </div>
               </section>
             </div>
             <div className={`${style.footer_contacto}`}>
-              <section>Quienes somos</section>
+              <section>
+                <p><Link>Quienes somos</Link></p>
+                <p><Link>Terminos y Condiciones</Link></p>
+                <p><Link>Privacidad</Link></p>
+                <p><Link>Top Libros</Link></p>
+                <p><Link>Contacto</Link></p>
+              </section>
             </div>
             <div className={`${style.footer_micuenta}`}>
-              <section>MI CUENTA</section>
+              <section>
+              <p>MI CUENTA</p>
+              <p><Link>Ingresar</Link></p>
+              <p><Link>Ver Carrito</Link></p>
+              <p><Link>Favoritos</Link></p>
+              <p><Link>Finalizar Compra</Link></p>
+              </section>
             </div>
             <div className={`${style.footer_seguinos}`}>
-              <section>SEGUINOS</section>
+              <h5>SEGUINOS</h5>
+              <section className={`${style.seguinos_logos}`}>
+                <img src={instagram} alt='instagram' />
+                <img src={youtube} alt="youtube" />
+                <img src={facebook} alt="facebook" />
+                <img src={twitter} alt="twitter" />
+              </section>
             </div>
           </div>
           <div className={`${style.footer_derechos}`}>
@@ -37,4 +74,4 @@ export class Footer extends Component {
   }
 }
 
-export default Footer
+export default Footer;
