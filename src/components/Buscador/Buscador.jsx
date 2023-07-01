3d6e43519c3385678b5a3591fdc4ac1029c59b0c
GiselaCapozzi <gisecapozzi@gmail.com>
2023-07-01 22:43:00 +0200

    Trayendo info de libros

--- src/components/Buscador/Buscador.jsx
+++ src/components/Buscador/Buscador.jsx
@@ -2,38 +2,77 @@ import { useState, useEffect } from 'react';
 import style from './Buscador.module.css';
 import CardBook from '../../container/CardBook/CardBook';
 import axios from 'axios';
+import ReactPaginate from 'react-paginate';
 
 const Buscador = () => {
 
   const [infoLibro, setInfoLibro] = useState([]);
   const [search, setSearch] = useState('');
+  const [productosPorPagina, setProductosPorPagina] = useState(50);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cantResultado, setCantResultado] = useState(); 
+  
+  const URL_API = `https://www.googleapis.com/books/v1/volumes?q=harry&key=AIzaSyAJlKP9GASA1rY442XsavTNlKXGcNnNR-c&maxResults=15`;
+  
+  const paginas = [];
 
-  const URL_API = 'https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=AIzaSyAJlKP9GASA1rY442XsavTNlKXGcNnNR-c';
+  const obtenerPaginas = Math.ceil(cantResultado/20);
+    
+  for (let i = 1; i <= obtenerPaginas; i++){
+    paginas.push(i);
+  }
 
-  const obtenerLibros = async () => {
+  const handleChange = e => {
+    setSearch(e.target.value);
+    // setInfoLibro([])
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     await axios.get(URL_API)
       .then(res => {
         setInfoLibro(res.data.items)
+        setCantResultado(res.data.totalItems)
       })
       .catch(error => {
-        console.log(error)
+        console.log(error.message)
       })
-  }
-  
-  useEffect(() => {
-    obtenerLibros();
-  }, [])
-
+    }
+    
+    useEffect(() => {
+      setInfoLibro()
+    }, [])
+    
+    console.log(infoLibro)
   return (
     <div className={`${style.container}`}>
-      <input className={` form-control ${style.input_search}`} type='search' />
-      <i className={`bi bi-search ${style.icon_search}`}></i>
-      <CardBook 
-        infoLibro={infoLibro}
-        key={infoLibro.id}
-      />
+      <form onSubmit={handleSubmit} className={`${style.buscador}`}>
+        <input
+          className={` form-control ${style.input_search}`}
+          type='search'
+          onChange={handleChange}
+          value={search}
+        />
+        <button
+          type='submit'
+          className={`btn btn-success`}
+        >Buscar</button>
+      </form>
+      {
+        search && infoLibro ? (
+          <div>
+            <CardBook
+              infoLibro={infoLibro}
+              key={infoLibro.id}
+              currentPage={currentPage}
+              productosPorPagina={productosPorPagina}
+            />
+          </div>
+        ) : (
+          <h4 className={`${style.sin_busqueda}`}>Realice una busqueda</h4>
+        )
+      }
       <div className={`${style.espacio}`}>
-        
       </div>
     </div>
   )
--- src/container/CardBook/CardBook.jsx
+++ src/container/CardBook/CardBook.jsx
@@ -2,12 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './CardBook.module.css';
 import sinPortada from '../../assets/sin_portada.jpg';
 
-const texto = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur quis velit bibendum, ullamcorper diam sed, facilisis ante. In cursus velit sem, aliquam fringilla est malesuada ut. Donec turpis lectus, auctor et nisi id, volutpat tempus urna. Donec iaculis at enim quis auctor. Maecenas tempor aliquet sem, feugiat pretium ante scelerisque vel. Aenean condimentum ipsum diam, nec viverra dolor tempor quis. Cras nec metus tincidunt, dignissim nulla sed, vehicula sapien. Ut pretium mauris non ipsum rhoncus volutpat. Aenean pretium arcu in mauris lobortis aliquam. Duis dictum lectus quam. Integer vitae faucibus nisi. '
-
 const CardBook = ({ infoLibro }) => {
-  console.log(infoLibro)
-  // const { title } = infoLibro.items[0].volumeInfo;
-  // console.log(title)
 
   return (
     <>
@@ -15,26 +10,29 @@ const CardBook = ({ infoLibro }) => {
         infoLibro ? (
           <div className={`${styles.container}`} >
             {
-              infoLibro.map(item => {
-                let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ;
-                return (
-                  <div key={item.id} className={`card ${styles.container_card}`}>
-                    <div className={`${styles.container_image}`}>
-                      <img src={thumbnail ? thumbnail : sinPortada} className={`card-img-top ${styles.portada}`} />
-                    </div>
-                    <div className={`card-body`}>
-                      <h5 className={`card-title ${styles.titulo}`}>{item.volumeInfo.title.length > 79 ? item.volumeInfo.title.slice(0, 80)+'...' : item.volumeInfo.title}</h5>
+              infoLibro.map(item => (
+                <div key={item.id} className={`card ${styles.container_card}`}>
+                  <div className={`${styles.container_image}`}>
+                    <img src={
+                      item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail ?
+                        item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.thumbnail :
+                        sinPortada}
+                      className={`card-img-top ${styles.portada}`} />
+                  </div>
+                  <div className={`card-body`}>
+                    <div className={`${styles.container_texto}`}>
+                      <h5 className={`card-title ${styles.titulo}`}>{item.volumeInfo.title.length > 79 ? item.volumeInfo.title.slice(0, 80) + '...' : item.volumeInfo.title}</h5>
                       <p className={`card-text ${styles.texto}`}>{item.volumeInfo.description ? item.volumeInfo.description.slice(0, 150) + '...' : '- Sin descripción -'}</p>
-                      <div className={`${styles.container_boton}`}>
-                        <Link to={`#`} className={`btn btn-primary ${styles.boton_mas}`}>Saber mas <div className={`${styles.container_boton}`}></div></Link>
-                      </div>
+                    </div>
+                    <div className={`${styles.container_boton}`}>
+                      <Link to={`#`} className={`btn btn-primary ${styles.boton_mas}`}>Saber mas <div className={`${styles.container_boton}`}></div></Link>
                     </div>
                   </div>
-                )
-              })
+                </div>
+              ))
             }
           </div>
-        ) : (<p>Realice una busqueda</p>)
+        ) : (<p>No se encontro la busqueda</p>)
       }
     </>
   )
