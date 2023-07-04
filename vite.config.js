e2b28f80298ccaa1194b4a35a6404deffb892206
GiselaCapozzi <gisecapozzi@gmail.com>
2023-07-04 02:33:49 +0200

    Solucionado un conflicto de peticion a Axios3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(
+  ({ command, mode }) => {
+    const port = 8080; const env = loadEnv(mode, process.cwd());
+
+    // console.log(comando: ${command} y modo: ${mode}); console.log("Variables de entorno:", env);
+
+    if (command === "development") { console.log("Modo desarrollo"); } else { console.log("Modo produccion"); }
+
+    return { server: { port }, },
+      { plugins: [react()] }
+  })
